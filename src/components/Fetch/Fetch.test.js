c31dfe01982566e05cfee747d2a940917abf5929
Alan Curtis <alancurtis5000@gmail.com>
2020-09-28 20:33:18 -0600

    test axios call fetching data

---
+++ src/components/Fetch/Fetch.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render, cleanup, waitForElement } from '@testing-library/react';
+import axiosMock from 'axios';
+import Fetch from './Fetch';
+
+describe('Fetch', () => {
+  afterEach(cleanup);
+
+  it('fetches and displays data', async () => {
+    axiosMock.get.mockResolvedValueOnce({ data: { greeting: 'hello there' } });
+    const url = '/greeting';
+    const { getByTestId } = render(<Fetch url={url} />);
+    expect(getByTestId('loading')).toHaveTextContent('Loading data...');
+
+    const resolvedSpan = await waitForElement(() => getByTestId('resolved'));
+    expect(resolvedSpan).toHaveTextContent('hello there');
+    expect(axiosMock.get).toHaveBeenCalledTimes(1);
+    expect(axiosMock.get).toHaveBeenCalledWith(url);
+  });
+});
